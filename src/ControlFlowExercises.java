59988f4e86326b1bcd042f087ee2196537f58332
Casanova Geary <CasanovaGeary@Gmail.com>
2022-10-10 12:37:18 -0500

    FIZZBUZZ: wrote a program printing numbers from 1 to 100.

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -32,7 +32,11 @@ public class ControlFlowExercises {
 //        for(int i = 100; i > -15; i -= 5) {
 //            System.out.println(i);
 //        }
-        for(long i = 2; i < 1000000; i *= i) {
+//        for(long i = 2; i < 1000000; i *= i) {
+//            System.out.println(i);
+//        }
+        // FIZZBUZZ
+        for(int i = 1; i <= 100; i++) {
             System.out.println(i);
         }
     }
