3f177b3c1666aa3c582d963233dd8b9f69450718
Casanova Geary <CasanovaGeary@Gmail.com>
2022-10-10 22:50:17 -0500

    Initial, and not clear with instructions.

---
+++ src/util/Input.java
@@ -0,0 +1,16 @@
+package util;
+
+import java.util.Scanner;
+
+public class Input {
+
+    private String scanner;
+    // When an instance of this object is created, the scanner field should be set to a new instance of the Scanner class.
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String getString
+    }
+
+}
