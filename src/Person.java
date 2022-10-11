b9c4907cf7ac9c63da3c3eabc949d8db1b2d6901
Casanova Geary <CasanovaGeary@Gmail.com>
2022-10-10 19:46:28 -0500

    Initial OOP exercise.

---
+++ src/Person.java
@@ -0,0 +1,16 @@
+public class Person {
+    // Private Fields
+    private String name;
+
+    public String getName() {
+//TODO: return the person's name
+        return name;
+    }
+
+    public void setName(String name) {
+//TODO: change the name field to the passed value
+    }
+    public void sayHello() {
+//TODO: print a message to the console using the person's name
+    }
+}
