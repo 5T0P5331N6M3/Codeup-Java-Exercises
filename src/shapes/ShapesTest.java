671e19c2066ff8e98d97520a82df99a4fd79daa6
Casanova Geary <CasanovaGeary@Gmail.com>
2022-10-14 00:57:31 -0500

    Break; battery is low and I need sleep.

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -18,10 +18,10 @@ public class ShapesTest
 
     /* Inheritance and Polymorphism Exercise Shapes.Rectangle, Shapes.Square, Shapes.ShapesTest */
 //  TODO: Test your code by creating a new class, ShapesTest (also inside of shapes) with a main method. Inside the main method:
-//  TODO: Create a variable of the type Rectangle named box1 and assign it a new instance of the Rectangle class with a width of 4 and a length of 5.
-//  TODO: Verify that the getPerimeter and getArea methods return 18 and 20, respectively.
-//  TODO: Create a variable of the type Rectangle named box2 and assign it a new instance of the Square class that has a side value of 5.
-//  TODO: Verify that the getPerimeter and getArea methods return 20 and 25, respectively.
+        //  TODO: Create a variable of the type Rectangle named box1 and assign it a new instance of the Rectangle class with a width of 4 and a length of 5.
+        //  TODO: Verify that the getPerimeter and getArea methods return 18 and 20, respectively.
+        //  TODO: Create a variable of the type Rectangle named box2 and assign it a new instance of the Square class that has a side value of 5.
+        //  TODO: Verify that the getPerimeter and getArea methods return 20 and 25, respectively.
 //  TODO: In the Square class, override the getArea and getPerimeter methods with the following definitions for a square:
 //  perimeter = 4 x side
 //  area = side ^ 2
