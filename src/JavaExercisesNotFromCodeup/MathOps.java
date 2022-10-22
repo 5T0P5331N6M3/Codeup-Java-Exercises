85d17436161bdb0846a097cc1e38e8517379665f
Casanova Geary <CasanovaGeary@Gmail.com>
2022-10-22 10:40:53 -0500

    Completed int a-d calcs.

--- src/JavaExercisesNotFromCodeup/MathOps.java
+++ src/JavaExercisesNotFromCodeup/MathOps.java
@@ -25,5 +25,22 @@ public class MathOps
         num2 = numInput.nextInt();
         int quotient = num1 / num2;
         System.out.println("The quotient of your input is " + quotient);
+
+        /* Write a Java program to print the result of the following operations:*/
+        int a = -5 + 8 * 6;
+        int b = (55 + 9) % 9;
+        int c = 20 + -3 * 5 / 8;
+        int d = 5 + 15 / 3 * 2 - 8 % 3;
+
+        System.out.println( -5 + 8 * 6);
+        System.out.println((55 + 9) % 9);
+        System.out.println(20 + -3 * 5 / 8);
+        System.out.println(5 + 15 / 3 * 2 - 8 % 3);
+        // Or
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+
     }
 }
