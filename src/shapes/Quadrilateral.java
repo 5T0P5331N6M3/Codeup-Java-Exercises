4dadf51618facc45b16bf8d71fddfb23f861c70a
Casanova Geary <CasanovaGeary@Gmail.com>
2022-10-25 16:56:02 -0500

    Completed Step 4.

--- src/shapes/Quadrilateral.java
+++ src/shapes/Quadrilateral.java
@@ -1,20 +1,26 @@
 package shapes;
 
-abstract class Quadrilateral extends Shape
+abstract class Quadrilateral extends Shape implements Measurable
 {
+    protected int length;
+    protected int width;
+
+    public Quadrilateral(int length, int width)
+    {
+        this.length = length;
+        this.width = width;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    abstract void setLength(int length);
+
+    abstract void setWidth(int width);
 
 }
-
-
-
-/*
-Inside of shapes, create an abstract Quadrilateral class that extends Shape and implements Measurable. This class should have:
-
-        protected properties for length and width.
-
-        a constructor that accepts two numbers for the length and width and sets those properties.
-
-        methods for getting the length and width.
-
-        abstract methods for setting the length and width.
-*/
