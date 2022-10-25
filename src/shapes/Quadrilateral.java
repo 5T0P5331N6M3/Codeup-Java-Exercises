6ac51489e29796d9cd160ebb96e9388071c63954
Casanova Geary <CasanovaGeary@Gmail.com>
2022-10-25 15:38:04 -0500

    Working on abstract classes.

---
+++ src/shapes/Quadrilateral.java
@@ -0,0 +1,20 @@
+package shapes;
+
+abstract class Quadrilateral extends Shape
+{
+
+}
+
+
+
+/*
+Inside of shapes, create an abstract Quadrilateral class that extends Shape and implements Measurable. This class should have:
+
+        protected properties for length and width.
+
+        a constructor that accepts two numbers for the length and width and sets those properties.
+
+        methods for getting the length and width.
+
+        abstract methods for setting the length and width.
+*/
--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,7 +1,5 @@
 package shapes;
 
-import java.util.Scanner;
-
 public class Rectangle
 {
 
