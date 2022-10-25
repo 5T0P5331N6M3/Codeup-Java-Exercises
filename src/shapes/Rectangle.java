48d0f57cb370037c19a23b2044dbd99fb40cb7b7
Casanova Geary <CasanovaGeary@Gmail.com>
2022-10-25 17:02:02 -0500

    Completed step 6.

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,6 +1,29 @@
 package shapes;
 
-public class Rectangle
+public class Rectangle extends Quadrilateral implements Measurable
 {
 
+    public Rectangle(int length, int width) {
+        super(length, width);
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 0;
+    }
+
+    @Override
+    public double getArea() {
+        return 0;
+    }
+
+    @Override
+    void setLength(int length) {
+
+    }
+
+    @Override
+    void setWidth(int width) {
+
+    }
 }
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,7 +1,29 @@
 package shapes;
 
-public class Square extends Rectangle
+public class Square extends Quadrilateral
 {
+    public Square(int length, int width) {
+        super(length, width);
+    }
 
+    @Override
+    public double getPerimeter() {
+        return 0;
+    }
+
+    @Override
+    public double getArea() {
+        return 0;
+    }
+
+    @Override
+    void setLength(int length) {
+
+    }
+
+    @Override
+    void setWidth(int width) {
+
+    }
 }
 
